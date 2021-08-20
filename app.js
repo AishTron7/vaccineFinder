75d784d1b50a8eb6bc3a1db697bdf73e40a4e6fa
Aishwary Shukla <aishwaryshukla@Starker7.local>
2021-08-20 18:30:15 +0530

    Added about section link

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const request = require("request");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
-const about1 = "This website is built to provide a clean and easy to use interface for the average user. Just open the website, enter your pincode, hit submit, and BAM! you get neat tabular data to answer all your vaccine slot related queries. Can't get simpler than that! 🤩";
+const about1 = "This website is built to provide a clean and easy to use interface for the average user. Just open the website, enter your pincode, hit submit, and BAM! you get neat tabular data to answer all your vaccine slot related queries. Searching for vaccine can't get simpler! 🤩";
 
 const about2 = "The Tech Stack of this website includes Node.js for backend and Embedded Javascript Templating (EJS) to give a consistent look across all the pages without repeating the header & footer code for every page. When the user submits the pincode, a well crafted query is sent as a GET request to the public Cowin API, which in return sends the JSON data about the vaccination centers. Then with due error handling, that data is parsed and displayed in a dynamic html table. This website is hosted on heroku and the codebase is available on github ";
 
